6c2280f04a9b65448d079a91ab295ebe7afde1d6
Глеб Агапов <baka@MacBook-Pro-Gleb.local>
2022-09-26 15:07:52 +0300

    Add

---
+++ prj.lab/queuep.cpp
@@ -0,0 +1,113 @@
+#include "queuep.h"
+
+QueueP::QueueP() noexcept
+{
+	head = nullptr;
+}
+
+QueueP::QueueP(const QueueP& p) noexcept {
+	auto temp = p.head.get();
+	while (temp) {
+		push(temp->num);
+		temp = temp->next.get();
+	}
+}
+
+QueueP::QueueP(QueueP&& p) noexcept : head(std::move(p.head)) { p.head = nullptr;}
+
+QueueP& QueueP::operator=(const QueueP& p)
+{
+	while (head)
+		head = std::move(head->next);
+
+	auto temp = p.head.get();
+	while (temp) {
+		push(temp->num);
+		temp = temp->next.get();
+	}
+	return *this;
+}
+
+QueueP::~QueueP()
+{
+	while (head)
+		head = std::move(head->next);
+}
+
+
+void QueueP::push(int data) noexcept {
+
+	if (head == nullptr || data < head.get()->num) {
+		head = std::unique_ptr<Node>(new Node{ data, std::move(head) });
+	}
+	else {
+		auto h = head.get();
+		while (h->next != nullptr && data >= h->next.get()->num)
+			h = h->next.get();
+		h->next = std::unique_ptr<Node>(new Node{ data, std::move(h->next) });
+	}
+}
+
+void QueueP::pop() noexcept
+{
+	if (head) {
+		head = std::move(head.get()->next);
+	}
+}
+
+int QueueP::top()
+{
+	if (head) {
+		return head.get()->num;
+	}
+	else {
+		throw std::out_of_range("Nullptr");
+	}
+}
+
+std::vector<int> QueueP::drop()
+{
+	std::vector<int> vec;
+	auto h = head.get();
+	while (h) {
+		vec.push_back(h->num);
+		h = h->next.get();
+	}
+	return vec;
+}
+
+void QueueP::print() noexcept {
+	auto h = head.get();
+	while (h) {
+		std::cout << h->num << " ";
+		h = h->next.get();
+	}
+
+}
+//------------------------------------------------------
+
+void print(std::vector<int> const& input)
+{
+	for (auto const& i : input) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+}
+
+QueueP init(int data)
+{
+	QueueP temp;
+	temp.push(data);
+	return temp;
+}
+
+
+void random(std::vector<int>& vec, QueueP& p, int& size) {
+	for (int i = 0; i < size; i++) {
+		int r = rand() % 1000;
+		vec[i] = r;
+		p.push(r);
+	}
+	std::sort(vec.begin(), vec.end());
+
+}
---
+++ prj.lab/queuep_test.cpp
@@ -0,0 +1,184 @@
+﻿#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "queuep.h"
+
+TEST_CASE("Push_random_1") {
+	QueueP q;
+	int size = 8;
+	std::vector<int> v(size);
+		
+	random(v,q,size);
+
+	CHECK(v == q.drop() );
+}
+
+TEST_CASE("Push_random_2") {
+	QueueP q;
+	int size = 16;
+	std::vector<int> v(size);
+
+	random(v, q, size);
+
+	CHECK(v == q.drop());
+}
+
+TEST_CASE("Push_random_3") {
+	QueueP q;
+	int size = 32;
+	std::vector<int> v(size);
+
+	random(v, q, size);
+
+	CHECK(v == q.drop());
+}
+
+TEST_CASE("Push_random_4") {
+	QueueP q;
+	int size = 64;
+	std::vector<int> v(size);
+
+	random(v, q, size);
+
+	CHECK(v == q.drop());
+}
+
+TEST_CASE("Push_random_5") {
+	QueueP q;
+	int size = 128;
+	std::vector<int> v(size);
+
+	random(v, q, size);
+
+	CHECK(v == q.drop());
+}
+
+TEST_CASE("Push_random_5") {
+	QueueP q;
+	int size = 128;
+	std::vector<int> v(size);
+
+	random(v, q, size);
+
+	CHECK(v == q.drop());
+}
+
+TEST_CASE("Push_random_6") {
+	QueueP q;
+	int size = 0;
+	std::vector<int> v(size);
+
+	random(v, q, size);
+
+	CHECK(v == q.drop());
+}
+
+TEST_CASE("top_random_1") {
+	QueueP q;
+	int size = 8;
+	std::vector<int> v(size);
+
+	random(v, q, size);
+
+	CHECK(v[0] == q.top());
+}
+
+TEST_CASE("top_random_2") {
+	QueueP q;
+	int size = 16;
+	std::vector<int> v(size);
+
+	random(v, q, size);
+
+	CHECK(v[0] == q.top());
+}
+
+TEST_CASE("top_random_3") {
+	QueueP q;
+	int size = 32;
+	std::vector<int> v(size);
+
+	random(v, q, size);
+
+	CHECK(v[0] == q.top());
+}
+
+TEST_CASE("top_random_4") {
+	QueueP q;
+	int size = 64;
+	std::vector<int> v(size);
+
+	random(v, q, size);
+
+	CHECK(v[0] == q.top());
+} 
+
+TEST_CASE("top_random_5") {
+	QueueP q;
+	int size = 128;
+	std::vector<int> v(size);
+
+	random(v, q, size);
+
+	CHECK(v[0] == q.top());
+}
+
+TEST_CASE("top_random_6") {
+	QueueP q;
+	int size = 0;
+	std::vector<int> v(size);
+
+	random(v, q, size);
+
+	CHECK_THROWS_AS(q.top(), const std::exception&);
+}
+
+TEST_CASE("ostream") {
+	QueueP q;
+	q.push(7);
+	q.push(1);
+	q.push(2);
+	q.push(10);
+	q.push(123);
+	q.push(12);
+
+	std::stringstream out;
+	out << q;
+
+	std::string get = out.str();
+
+	std::string result = "1 2 7 10 12 123";
+
+	CHECK(get == result);
+}
+
+TEST_CASE("istream") {
+	QueueP p;
+	std::vector<int> vec{5,10,1,4,12313,534,0,-100};
+	std::stringstream in, out;
+	std::string result = "-100 0 1 4 5 10 534 12313";
+
+	for (int i = 0; i < vec.size(); i++) {
+		vec[i] == vec[vec.size()-1]? in << vec[i] << " " : in << vec[i] << " ";
+	}
+
+	in >> p;
+	out << p;
+
+	std::string get = out.str();
+
+	CHECK(get == result);
+}
+
+TEST_CASE("copy") {
+	QueueP r;
+	r.push(123);
+	r.push(4);
+	r.push(-19);
+	r.push(100);
+	r.push(-10);
+
+	QueueP p(init(10));
+	p.print();
+	std::cout << std::endl;
+	r.print();
+}
